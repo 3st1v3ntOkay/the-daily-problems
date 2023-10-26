dcef5e13169a614cef2024f4731edec0e481aec0
Esteban Jimenez Valdez <wiwiwixd732@gmail.com>
2023-10-26 15:27:40 -0700

    feat: Container With Most Water

--- leetcode/two-pointers/container-w-most-water.js
+++ leetcode/two-pointers/container-w-most-water.js
@@ -2,6 +2,26 @@
  * @param {number[]} height
  * @return {number}
  */
-const maxArea = function (height) {};
+const maxArea = function (height) {
+  if (height.length < 2) return height;
+
+  let l = 0;
+  let r = height.length - 1;
+  let res = 0;
+
+  while (l < r) {
+    let area = (r - l) * Math.min(height[l], height[r]);
+    res = Math.max(res, area);
+
+    if (height[l] < height[r]) {
+      l += 1;
+    } else {
+      r -= 1;
+    }
+  }
+
+  console.log(res);
+  return res;
+};
 
 maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]);
