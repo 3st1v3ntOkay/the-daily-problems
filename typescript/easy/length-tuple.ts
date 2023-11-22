f4b32bcc826dc15408ca1ffa65097175b43b302e
Esteban <wiwiwixd732@gmail.com>
2023-11-22 13:13:09 -0800

    feat: length of tuple challenge done

---
+++ typescript/easy/length-tuple.ts
@@ -0,0 +1,16 @@
+type Length<T extends readonly any[]> = T["length"]
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+const tesla = ['tesla', 'model 3', 'model X', 'model Y'] as const
+const spaceX = ['FALCON 9', 'FALCON HEAVY', 'DRAGON', 'STARSHIP', 'HUMAN SPACEFLIGHT'] as const
+
+type cases = [
+  Expect<Equal<Length<typeof tesla>, 4>>,
+  Expect<Equal<Length<typeof spaceX>, 5>>,
+  // @ts-expect-error
+  Length<5>,
+  // @ts-expect-error
+  Length<'hello world'>,
+]
