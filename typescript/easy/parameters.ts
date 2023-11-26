2a11054749017aaab3c97fee5f22a29e12c1b179
Esteban <wiwiwixd732@gmail.com>
2023-11-26 15:06:57 -0800

    feat: last 3 chanllenges (parameters, push, unshift)

---
+++ typescript/easy/parameters.ts
@@ -0,0 +1,18 @@
+// @motionrus: https://github.com/type-challenges/type-challenges/issues/10444
+
+type MyParameters<T extends (...args: any[]) => void> =
+  T extends (...args: infer values) => void
+    ? [...values]
+    : []
+
+import type { Equal, Expect } from '@type-challenges/utils'
+
+const foo = (arg1: string, arg2: number): void => {}
+const bar = (arg1: boolean, arg2: { a: 'A' }): void => {}
+const baz = (): void => {}
+
+type cases = [
+  Expect<Equal<MyParameters<typeof foo>, [string, number]>>,
+  Expect<Equal<MyParameters<typeof bar>, [boolean, { a: 'A' }]>>,
+  Expect<Equal<MyParameters<typeof baz>, []>>,
+]
