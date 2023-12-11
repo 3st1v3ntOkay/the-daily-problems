0a8e387df6e2bacf47ccc33a21d93422ed4e004c
Esteban <wiwiwixd732@gmail.com>
2023-12-11 11:51:21 -0800

    feat + error: drop char challenge finished + check why is that error happen

---
+++ typescript/medium/drop-char.ts
@@ -0,0 +1,22 @@
+type DropChar<
+  S extends string,
+  C extends string
+> =
+  S extends `${infer head}${infer rest}`
+  ? head extends C
+  ? DropChar<rest, C>
+  : `${head}${DropChar<rest, C>}`
+  : S
+
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  // @ts-expect-error
+  Expect<Equal<DropChar<'butter fly!', ''>, 'butterfly!'>>,
+  Expect<Equal<DropChar<'butter fly!', ' '>, 'butterfly!'>>,
+  Expect<Equal<DropChar<'butter fly!', '!'>, 'butter fly'>>,
+  Expect<Equal<DropChar<'    butter fly!        ', ' '>, 'butterfly!'>>,
+  Expect<Equal<DropChar<' b u t t e r f l y ! ', ' '>, 'butterfly!'>>,
+  Expect<Equal<DropChar<' b u t t e r f l y ! ', 'b'>, '  u t t e r f l y ! '>>,
+  Expect<Equal<DropChar<' b u t t e r f l y ! ', 't'>, ' b u   e r f l y ! '>>,
+]
