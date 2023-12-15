69007c0eb5fc23c58b35609c63ac43c2fee2d36e
Esteban <wiwiwixd732@gmail.com>
2023-12-15 11:54:02 -0800

    feat: challenge completed

---
+++ typescript/medium/reverse.ts
@@ -0,0 +1,19 @@
+type Reverse<T extends any[]> =
+  T extends [...infer left, infer right]
+  ? [right, ...Reverse<left>]
+  : T;
+
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<Reverse<[]>, []>>,
+  Expect<Equal<Reverse<['a', 'b']>, ['b', 'a']>>,
+  Expect<Equal<Reverse<['a', 'b', 'c']>, ['c', 'b', 'a']>>,
+]
+
+type errors = [
+  // @ts-expect-error
+  Reverse<'string'>,
+  // @ts-expect-error
+  Reverse<{ key: 'value' }>,
+]
