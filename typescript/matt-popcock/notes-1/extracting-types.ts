945cb9e6a0820ac9f9dc8367b6cf01b05e76e95a
Esteban <wiwiwixd732@gmail.com>
2024-01-06 10:57:14 -0800

    feat: nothing important

--- typescript/matt-popcock/notes-1/extracting-types.ts
+++ typescript/matt-popcock/notes-1/extracting-types.ts
@@ -3,4 +3,5 @@ type RolesArray = typeof roles;
 
 // type Roles = RolesArray[0] | RolesArray[1] | RolesArray[2] | RolesArray[3];
 // type Roles = RolesArray[0 | 1 | 2 | 3];
-type Roles = RolesArray[number];
+type Roles1 = RolesArray[number];
+type Roles2 = typeof roles[number];
