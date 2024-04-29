194a2d41d95b18ddfee292819059d52f3953a363
Esteban <wiwiwixd732@gmail.com>
2024-04-29 15:22:18 -0700

    feat: new challenge complete

---
+++ typescript/hard/is-negative-number.ts
@@ -0,0 +1,35 @@
+import type { Equal, Expect } from "@type-challenges/utils";
+
+type myIsNegativeNumber<T extends number> =
+  `${T}` extends `${infer NegativeSymbol}${string}`
+  ? NegativeSymbol extends '-'
+  ? true
+  : false
+  : false;
+
+// @jazelly
+type IsNegativeNumber<
+  T extends number,
+  U extends T = T,
+> =
+  number extends T
+  ? never
+  : T extends U
+    ? [U] extends [T]
+      ? `${T}` extends `-${infer _}`
+        ? true
+        : false
+      : never
+    : never;
+
+type cases = [
+  Expect<Equal<IsNegativeNumber<0>, false>>,
+  Expect<Equal<IsNegativeNumber<number>, never>>,
+  Expect<Equal<IsNegativeNumber<-1 | -2>, never>>,
+  Expect<Equal<IsNegativeNumber<-1>, true>>,
+  Expect<Equal<IsNegativeNumber<-1.9>, true>>,
+  Expect<Equal<IsNegativeNumber<-100_000_000>, true>>,
+  Expect<Equal<IsNegativeNumber<1>, false>>,
+  Expect<Equal<IsNegativeNumber<1.9>, false>>,
+  Expect<Equal<IsNegativeNumber<100_000_000>, false>>,
+];
