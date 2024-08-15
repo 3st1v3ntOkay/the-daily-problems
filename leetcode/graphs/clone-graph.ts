52b794c10df6aad110b82c815f6650cc3a7788b1
Esteban Jimenez Valdez <88903033+3st1v3ntOkay@users.noreply.github.com>
2024-08-15 15:06:48 -0700

    feat: init clone graph problem

---
+++ leetcode/graphs/clone-graph.ts
@@ -0,0 +1,13 @@
+class _Node {
+  val: number;
+  neighbors: _Node[];
+
+  constructor(val?: number, neighbors?: _Node[]) {
+    this.val = (val === undefined ? 0 : val);
+    this.neighbors = (neighbors === undefined ? [] : neighbors);
+  }
+}
+
+function cloneGraph(node: _Node | null): _Node | null {
+  return null;
+}
