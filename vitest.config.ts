31b75b38dd7b3cc9ec5c9b760b00b40b71ece430
Esteban Jimenez Valdez <88903033+3st1v3ntOkay@users.noreply.github.com>
2024-10-10 09:46:46 -0700

    feat: alias path added

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,13 @@
 import { defineConfig } from "vitest/config";
+import { resolve } from "node:path";
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@leetcode": resolve(__dirname, "./leetcode"),
+      "@typescript": resolve(__dirname, "./typescript"),
+    },
+  },
   test: {
     environment: "node",
   },
