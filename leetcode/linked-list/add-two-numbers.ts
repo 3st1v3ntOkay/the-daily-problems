aed78599bf15ef5e91b2a327961f5b9bb20a99ac
Esteban Jimenez Valdez <88903033+3st1v3ntOkay@users.noreply.github.com>
2024-11-16 12:50:54 -0800

    feat: file chnaged from js to ts

--- leetcode/linked-list/add-two-numbers.ts
+++ leetcode/linked-list/add-two-numbers.ts
@@ -1,16 +1,9 @@
-class ListNode {
-  constructor(val, next) {
-    this.val = (val === undefined ? 0 : val);
-    this.next = (next === undefined ? null : next);
-  }
-}
+import {
+  LinkedList as ListNode,
+  addList,
+} from "./data/LinkedList"
 
-/**
- * @param {ListNode} l1
- * @param {ListNode} l2
- * @return {ListNode}
- */
-const myAddTwoNumbers = function(l1, l2) {
+const myAddTwoNumbers = function (l1: any, l2: any) {
   let stack1 = [], stack2 = [];
 
   while (l1) {
@@ -22,9 +15,9 @@ const myAddTwoNumbers = function(l1, l2) {
     stack2.unshift(l2.val);
     l2 = l2.next;
   }
-  
+
   const sum = Number(stack1.join("")) + Number(stack2.join(""))
-  
+
   const newArray = []
   for (let value of String(sum)) {
     newArray.unshift(value)
@@ -34,8 +27,8 @@ const myAddTwoNumbers = function(l1, l2) {
   let res = null
   let pivot = null
   while (newArray.length) {
-    let node = new ListNode(newArray.shift());
-    
+    let node = new LinkedList(newArray.shift());
+
     if (res) {
       pivot.next = node;
       pivot = pivot.next;
@@ -44,17 +37,15 @@ const myAddTwoNumbers = function(l1, l2) {
       pivot = res;
     }
   }
-  
+
   console.log(res)
   return res
 }; // it just fail for 3 test cases
 
-/**
- * @param {ListNode} l1
- * @param {ListNode} l2
- * @return {ListNode}
- */
-const addTwoNumbers = function(l1, l2) {
+const addTwoNumbers = function (
+  l1: ListNode | null,
+  l2: ListNode | null,
+): ListNode | null {
   // @neetcode video
   let dummy = new ListNode(),
     current = dummy;
@@ -69,7 +60,7 @@ const addTwoNumbers = function(l1, l2) {
     val = val % 10;
 
     current.next = new ListNode(val);
-    
+
     current = current.next;
     l1 = l1 ? l1.next : null;
     l2 = l2 ? l2.next : null;
@@ -80,25 +71,33 @@ const addTwoNumbers = function(l1, l2) {
     current.next = new ListNode(carry);
   }
 
-  console.log(dummy.next);
   return dummy.next;
 }
 
-const a = new ListNode(2);
-const b = new ListNode(4);
-const c = new ListNode(3);
-
-a.next = b;
-b.next = c;
-
-const d = new ListNode(5);
-const e = new ListNode(6);
-const f = new ListNode(4);
-
-d.next = e;
-e.next = f;
-
-
-const l1 = a, l2 = d;
+const l1: ListNode = addList(2, 4, 3);
+const l2: ListNode = addList(5, 6, 4);
 
 addTwoNumbers(l1, l2);
+
+
+/* dummie way, pffff
+
+  const a = new ListNode(2);
+  const b = new ListNode(4);
+  const c = new ListNode(3);
+
+  a.next = b;
+  b.next = c;
+
+  const d = new ListNode(5);
+  const e = new ListNode(6);
+  const f = new ListNode(4);
+
+  d.next = e;
+  e.next = f;
+
+
+  const l1 = a, l2 = d;
+
+  addTwoNumbers(l1, l2);
+*/
