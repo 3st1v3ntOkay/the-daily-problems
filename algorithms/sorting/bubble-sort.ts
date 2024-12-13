52cb0280a68df8d8fe6936153087023f564d1a28
Esteban Jimenez Valdez <88903033+3st1v3ntOkay@users.noreply.github.com>
2024-12-13 13:33:36 -0800

    feat: +1 new bubble sort variant + some minor changes

--- algorithms/sorting/bubble-sort.ts
+++ algorithms/sorting/bubble-sort.ts
@@ -1,32 +1,52 @@
 const BubbleNums: number[] = [3, 1, 2, 4];
 
-function BubbleSort(nums: number[]): number[] {
-  if (!nums.length) return [];
+function BubbleSort(array: number[]): number[] {
+  if (!array.length) return [];
 
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = 0; j < nums.length; j++) {
-      if (nums[i] < nums[j]) {
-        [nums[i], nums[j]] = [nums[j], nums[i]]
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      if (array[i] < array[j]) {
+        [array[i], array[j]] = [array[j], array[i]]
       }
     }
   }
 
-  return nums;
+  return array;
 }
 
-function BubbleSortVariant1(nums: number[]): number[] {
-  if (!nums.length) return [];
+function BubbleSortVariant1(array: number[]): number[] {
+  if (!array.length) return [];
 
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = 0; j < nums.length; j++) {
-      if (nums[j] > nums[j + 1]) {
-        [nums[i], nums[j]] = [nums[j], nums[i]]
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      if (array[j] > array[j + 1]) {
+        [array[j], array[j + 1]] = [array[j + 1], array[j]];
       }
     }
   }
 
-  return nums;
+  return array;
+}
+
+function BubbleSortVariant2(array: number[]): number[] {
+  if (!array.length) return [];
+
+  let temp: number;
+
+  for (let k = 0; k < array.length; k++) {
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] > array[i + 1]) {
+        temp = array[i];
+        array[i] = array[i + 1];
+        array[i + 1] = temp;
+      }
+    }
+  }
+
+  return array;
 }
 
 BubbleSort(BubbleNums);
+
 BubbleSortVariant1(BubbleNums);
+BubbleSortVariant2(BubbleNums);
