fa129798eea75d0b404742ba3a27ea44c3ec90d4
Esteban Jimenez Valdez <88903033+3st1v3ntOkay@users.noreply.github.com>
2025-01-02 16:56:24 -0800

    major: re-construction of the project

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,12 +2,12 @@ import { defineConfig } from "vitest/config";
 import { resolve } from "node:path";
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      "@leetcode": resolve(__dirname, "./leetcode"),
-      "@typescript": resolve(__dirname, "./typescript"),
-    },
-  },
+  // resolve: {
+  //   alias: {
+  //     "@leetcode": resolve(__dirname, "./leetcode"),
+  //     "@typescript": resolve(__dirname, "./typescript"),
+  //   },
+  // },
   test: {
     environment: "node",
   },
